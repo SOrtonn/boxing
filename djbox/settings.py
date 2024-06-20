1f9d33b95e124b7237fc31f571e94f9c7afee603
sam <samorton46@gmail.com>
2024-06-20 20:02:43 +0100

    fixing static file thing v4

--- djbox/settings.py
+++ djbox/settings.py
@@ -121,13 +121,16 @@ USE_TZ = True
 
 #STATIC_URL = 'static/'
 import os
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-]
-
+#//STATICFILES_DIRS = [
+#    BASE_DIR / "static",
+#]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
