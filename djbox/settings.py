6934d6cd7ce4feee7c05bdc2d89dcc6613507a43
sam <samorton46@gmail.com>
2024-06-20 19:55:55 +0100

    fixing static file thing

--- djbox/settings.py
+++ djbox/settings.py
@@ -120,6 +120,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 #STATIC_URL = 'static/'
+import os
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     BASE_DIR / "static",
